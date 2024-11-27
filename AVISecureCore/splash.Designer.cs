83abf5aa4c656a68efacab2cfff7945dec6943c7
Leandro Varas <leovaras2005@gmail.com>
2024-11-27 17:42:26 +0100

    refactor: El video se inicia en pantalla completa

--- AVISecureCore/splash.Designer.cs
+++ AVISecureCore/splash.Designer.cs
@@ -37,23 +37,23 @@ namespace AVISecureCore
             // backVideo
             // 
             this.backVideo.Enabled = true;
-            this.backVideo.Location = new System.Drawing.Point(-2, -1);
+            this.backVideo.Location = new System.Drawing.Point(-4, -3);
             this.backVideo.Name = "backVideo";
             this.backVideo.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("backVideo.OcxState")));
-            this.backVideo.Size = new System.Drawing.Size(1296, 766);
+            this.backVideo.Size = new System.Drawing.Size(1924, 1128);
             this.backVideo.TabIndex = 0;
-            this.backVideo.Enter += new System.EventHandler(this.backVideo_Enter);
             // 
             // splash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
             this.Controls.Add(this.backVideo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "splash";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "splash";
+            this.Load += new System.EventHandler(this.splash_Load);
             ((System.ComponentModel.ISupportInitialize)(this.backVideo)).EndInit();
             this.ResumeLayout(false);
 
--- AVISecureCore/splash.cs
+++ AVISecureCore/splash.cs
@@ -17,11 +17,13 @@ namespace AVISecureCore
             InitializeComponent();
         }
 
-        private void backVideo_Enter(object sender, EventArgs e)
-        {
-            backVideo.URL = @"C:\DAM2n\ABP\ProyectoABP\resourses\splashVideo.mp4";
-            backVideo.Ctlcontrols.play();
-        }
+        //private void backVideo_Enter(object sender, EventArgs e)
+        //{
+        //    backVideo.URL = @"C:\DAM2n\ABP\ProyectoABP\resourses\video-splash.mp4";
+        //    backVideo.fullScreen = true;
+        //    backVideo.Ctlcontrols.play();
+            
+        //}
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
         {
             if (keyData == (Keys.Escape))
@@ -31,6 +33,23 @@ namespace AVISecureCore
             }
 
             return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void splash_Load(object sender, EventArgs e)
+        {
+            backVideo.URL = @"C:\DAM2n\ABP\ProyectoABP\resourses\video-splash.mp4";
+            backVideo.settings.autoStart = true;
+            backVideo.uiMode = "none"; // Ocultar controles
+            backVideo.PlayStateChange += BackVideo_PlayStateChange; // Suscribirse al evento
+        }
+
+        private void BackVideo_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            // Verificar si el estado del reproductor es "Playing"
+            if (e.newState == (int)WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                backVideo.fullScreen = true; // Hacer pantalla completa
+            }
         }
     }
 }
