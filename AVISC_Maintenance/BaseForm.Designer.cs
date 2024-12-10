eb8218583e7e59e024c316a9f163b5f7770a9ef9
EnricMart <enric.moben@gmail.com>
2024-12-10 18:28:31 +0100

    k

---
+++ AVISC_Maintenance/BaseForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace AVISC_Maintenance
+{
+    partial class BaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.swTextbox1 = new AVISC_Controles.SWTextbox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // swTextbox1
+            // 
+            this.swTextbox1.AllowEmpty = false;
+            this.swTextbox1.DadaPermesa = AVISC_Controles.SWTextbox.ContentType.Nombre;
+            this.swTextbox1.IsForeignKey = false;
+            this.swTextbox1.LinkedControlName = null;
+            this.swTextbox1.Location = new System.Drawing.Point(123, 75);
+            this.swTextbox1.Name = "swTextbox1";
+            this.swTextbox1.Size = new System.Drawing.Size(201, 22);
+            this.swTextbox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(366, 358);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // BaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1822, 953);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.swTextbox1);
+            this.Name = "BaseForm";
+            this.Text = "BaseForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AVISC_Controles.SWTextbox swTextbox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
