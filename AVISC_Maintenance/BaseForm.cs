03be4faa85525ae663c2e40991e50a12cf9d354f
EnricMart <enric.moben@gmail.com>
2024-12-11 19:48:01 +0100

    skibididopsalenLasRows

--- AVISC_Maintenance/BaseForm.cs
+++ AVISC_Maintenance/BaseForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using AVIDataAccess;
@@ -12,13 +13,15 @@ using AVIDataAccess;
 
 namespace AVISC_Maintenance
 {
-    public class MaintenanceDataAccess : DataAccess { };
+   
 
     public partial class BaseForm : Form
     {
+        public class MaintenanceDataAccess : DataAccess { };
         private MaintenanceDataAccess dataAccess;
         private DataSet dts;
         public  string taula { get; set; }
+        private Dictionary<string, string> fields = new Dictionary<string, string>();
 
         public BaseForm()
         {
@@ -26,20 +29,56 @@ namespace AVISC_Maintenance
             InitializeComponent();
             PortarDates();
         }
-
+        bool EsNou = false;
         private void PortarDates()
         {
 
-            dts = dataAccess.PortarPerConsulta( "Select * from Species" , " Species ");
+            dts = dataAccess.PortarTaula(" Species ");
             dataGridView1.DataSource = dts.Tables["Species"];
+            Regex compCampoId = new Regex("(?i)id");
+            if(dts !=null && dts.Tables.Count >0)
+            {
+                dataGridView1.DataSource = dts.Tables[0];
+                foreach (DataGridViewColumn column in dataGridView1.Columns)
+                {
+                    if (compCampoId.IsMatch(column.Name))
+                    {
+                        column.Visible = false;
+                    }
+                }
+            }
 
 
 
         }
-       
+
         private void actualizarDB_Click(object sender, EventArgs e)
         {
+            if (EsNou)
+            {
+                DataRow newRow = dts.Tables["Species"].NewRow();
+                newRow["CodeSpecie"] = swTextbox1.Text;
+                newRow["DescSpecie"] = swTextbox3.Text;
 
+                dts.Tables["Species"].Rows.Add(newRow);
+                EsNou = false;
+            }
+
+            string querySelect = "SELECT * FROM Species";
+            
+            foreach  (Control item in this.Controls)
+            {
+                if(item is TextBox)
+                {
+                    TextBox ctr = (TextBox)item;
+                    fields.Add(item.Tag.ToString(), item.Text);
+                }
+            }
+            int result = dataAccess.Actualitzar(dts, querySelect , fields );
+
+            MessageBox.Show($"Registros modificados: {result}");
+
+            PortarDates();
         }
 
         private void nuevoDB_Click(object sender, EventArgs e)
