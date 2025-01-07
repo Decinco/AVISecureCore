7a73e385c1a886fcbb35e0bd0475b796ae9d5fdc
EnricMart <enric.moben@gmail.com>
2025-01-07 17:59:44 +0100

    ad

---
+++ MantOpcions/MantOpcions.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace MenuOptions
+{
+    partial class MantOpcions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ensamblat = new System.Windows.Forms.TextBox();
+            this.Classe = new System.Windows.Forms.TextBox();
+            this.Nom = new System.Windows.Forms.TextBox();
+            this.Descripcio = new System.Windows.Forms.TextBox();
+            this.lblEnsamblat = new System.Windows.Forms.Label();
+            this.lblClasse = new System.Windows.Forms.Label();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Ensamblat
+            // 
+            this.Ensamblat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.Ensamblat.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Ensamblat.ForeColor = System.Drawing.Color.White;
+            this.Ensamblat.Location = new System.Drawing.Point(33, 84);
+            this.Ensamblat.Multiline = true;
+            this.Ensamblat.Name = "Ensamblat";
+            this.Ensamblat.Size = new System.Drawing.Size(245, 60);
+            this.Ensamblat.TabIndex = 31;
+            this.Ensamblat.Tag = "Ensamblat";
+            // 
+            // Classe
+            // 
+            this.Classe.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.Classe.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Classe.ForeColor = System.Drawing.Color.White;
+            this.Classe.Location = new System.Drawing.Point(357, 84);
+            this.Classe.Multiline = true;
+            this.Classe.Name = "Classe";
+            this.Classe.Size = new System.Drawing.Size(245, 60);
+            this.Classe.TabIndex = 32;
+            this.Classe.Tag = "Classe";
+            // 
+            // Nom
+            // 
+            this.Nom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.Nom.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Nom.ForeColor = System.Drawing.Color.White;
+            this.Nom.Location = new System.Drawing.Point(682, 84);
+            this.Nom.Multiline = true;
+            this.Nom.Name = "Nom";
+            this.Nom.Size = new System.Drawing.Size(245, 60);
+            this.Nom.TabIndex = 33;
+            this.Nom.Tag = "Nom";
+            // 
+            // Descripcio
+            // 
+            this.Descripcio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.Descripcio.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Descripcio.ForeColor = System.Drawing.Color.White;
+            this.Descripcio.Location = new System.Drawing.Point(1006, 84);
+            this.Descripcio.Multiline = true;
+            this.Descripcio.Name = "Descripcio";
+            this.Descripcio.Size = new System.Drawing.Size(245, 60);
+            this.Descripcio.TabIndex = 34;
+            this.Descripcio.Tag = "Descripcio";
+            // 
+            // lblEnsamblat
+            // 
+            this.lblEnsamblat.AutoSize = true;
+            this.lblEnsamblat.Font = new System.Drawing.Font("Inter", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnsamblat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(241)))), ((int)(((byte)(102)))));
+            this.lblEnsamblat.Location = new System.Drawing.Point(33, 19);
+            this.lblEnsamblat.Name = "lblEnsamblat";
+            this.lblEnsamblat.Size = new System.Drawing.Size(201, 50);
+            this.lblEnsamblat.TabIndex = 36;
+            this.lblEnsamblat.Text = "Ensamblat";
+            // 
+            // lblClasse
+            // 
+            this.lblClasse.AutoSize = true;
+            this.lblClasse.Font = new System.Drawing.Font("Inter", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClasse.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(241)))), ((int)(((byte)(102)))));
+            this.lblClasse.Location = new System.Drawing.Point(348, 19);
+            this.lblClasse.Name = "lblClasse";
+            this.lblClasse.Size = new System.Drawing.Size(138, 50);
+            this.lblClasse.TabIndex = 37;
+            this.lblClasse.Text = "Classe";
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Font = new System.Drawing.Font("Inter", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNom.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(241)))), ((int)(((byte)(102)))));
+            this.lblNom.Location = new System.Drawing.Point(673, 19);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(102, 50);
+            this.lblNom.TabIndex = 38;
+            this.lblNom.Text = "Nom";
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Font = new System.Drawing.Font("Inter", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesc.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(241)))), ((int)(((byte)(102)))));
+            this.lblDesc.Location = new System.Drawing.Point(997, 19);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(205, 50);
+            this.lblDesc.TabIndex = 39;
+            this.lblDesc.Text = "Descripció";
+            // 
+            // MantOpcions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1572, 888);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.lblNom);
+            this.Controls.Add(this.lblClasse);
+            this.Controls.Add(this.lblEnsamblat);
+            this.Controls.Add(this.Descripcio);
+            this.Controls.Add(this.Nom);
+            this.Controls.Add(this.Classe);
+            this.Controls.Add(this.Ensamblat);
+            this.Name = "MantOpcions";
+            this.Text = "MantOpcions";
+            this.Controls.SetChildIndex(this.Ensamblat, 0);
+            this.Controls.SetChildIndex(this.Classe, 0);
+            this.Controls.SetChildIndex(this.Nom, 0);
+            this.Controls.SetChildIndex(this.Descripcio, 0);
+            this.Controls.SetChildIndex(this.lblEnsamblat, 0);
+            this.Controls.SetChildIndex(this.lblClasse, 0);
+            this.Controls.SetChildIndex(this.lblNom, 0);
+            this.Controls.SetChildIndex(this.lblDesc, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Ensamblat;
+        private System.Windows.Forms.TextBox Classe;
+        private System.Windows.Forms.TextBox Nom;
+        private System.Windows.Forms.TextBox Descripcio;
+        private System.Windows.Forms.Label lblEnsamblat;
+        private System.Windows.Forms.Label lblClasse;
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.Label lblDesc;
+    }
+}
