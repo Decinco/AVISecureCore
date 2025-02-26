c8644dc01c7e4086c7b57266aeae51b5d399e1f2
teropod24 <jx.paredes@sarria.salesians.cat>
2025-02-26 19:55:05 +0100

    Añadir codigo de leer el edi, no acabado

---
+++ AVISC_EdiTools/EDI.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace AVISC_EdiTools
+{
+    partial class EDI
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.swTextButton1 = new AVISC_Controles.SWTextButton();
+            this.swTextButton2 = new AVISC_Controles.SWTextButton();
+            this.txt_Edi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // swTextButton1
+            // 
+            this.swTextButton1.ButtonEnabled = true;
+            this.swTextButton1.ButtonText = "Seleccionar el EDI";
+            this.swTextButton1.Location = new System.Drawing.Point(85, 209);
+            this.swTextButton1.Name = "swTextButton1";
+            this.swTextButton1.Size = new System.Drawing.Size(262, 63);
+            this.swTextButton1.TabIndex = 25;
+            this.swTextButton1.ButtonClick += new System.EventHandler(this.swTextButton1_ButtonClick);
+            // 
+            // swTextButton2
+            // 
+            this.swTextButton2.ButtonEnabled = true;
+            this.swTextButton2.ButtonText = "Subir a la BDD";
+            this.swTextButton2.Location = new System.Drawing.Point(734, 219);
+            this.swTextButton2.Name = "swTextButton2";
+            this.swTextButton2.Size = new System.Drawing.Size(262, 63);
+            this.swTextButton2.TabIndex = 26;
+            this.swTextButton2.ButtonClick += new System.EventHandler(this.swTextButton2_ButtonClick);
+            // 
+            // txt_Edi
+            // 
+            this.txt_Edi.Location = new System.Drawing.Point(399, 209);
+            this.txt_Edi.Multiline = true;
+            this.txt_Edi.Name = "txt_Edi";
+            this.txt_Edi.Size = new System.Drawing.Size(262, 499);
+            this.txt_Edi.TabIndex = 27;
+            // 
+            // EDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1840, 1000);
+            this.Controls.Add(this.txt_Edi);
+            this.Controls.Add(this.swTextButton2);
+            this.Controls.Add(this.swTextButton1);
+            this.Name = "EDI";
+            this.Text = "Form1";
+            this.Controls.SetChildIndex(this.swTextButton1, 0);
+            this.Controls.SetChildIndex(this.swTextButton2, 0);
+            this.Controls.SetChildIndex(this.txt_Edi, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AVISC_Controles.SWTextButton swTextButton1;
+        private AVISC_Controles.SWTextButton swTextButton2;
+        private System.Windows.Forms.TextBox txt_Edi;
+    }
+}
+
---
+++ AVISC_EdiTools/EDI.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AVISC_BaseForms;
+
+namespace AVISC_EdiTools
+{
+    public partial class EDI : AVISC_CloseableFeatureForm
+    {
+        SecureCoreG5Entities sc;
+        List<Orders> orders;
+        List<Priority> priority;
+        List<OperationalAreas> operationalAreas;
+        List<Agencies> agencies;
+        List<Factories> factories;
+        List<OrderInfo> orderInfo;
+        List<OrdersDetail> ordersDetail;
+        List<Planets> planets;
+        List<References> references;
+        public EDI()
+        {
+            InitializeComponent();
+        }
+
+        private void swTextButton1_ButtonClick(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.ShowDialog();
+
+                sc = new SecureCoreG5Entities();
+                orders = new List<Orders>();
+                priority = new List<Priority>();
+                operationalAreas = new List<OperationalAreas>();
+                agencies = new List<Agencies>();
+                factories = new List<Factories>();
+                orderInfo = new List<OrderInfo>();
+                ordersDetail = new List<OrdersDetail>();
+                planets = new List<Planets>();
+                references = new List<References>();            
+
+                string[] docRead = File.ReadAllLines(openFileDialog.FileName);
+
+                bool correcte = false;
+
+                if (openFileDialog.FileName.Length > 0)
+                {
+                    using (StreamReader streamReader = new StreamReader(openFileDialog.FileName))
+                    {
+                        foreach (string line in docRead)
+                        {
+                            List<string> parts = line.Split('|').ToList();
+                            int cantidad;
+                            
+                            if (parts[0] == "ORDERS_D_96A_UN_EAN008")
+                            {
+                                correcte = true;
+                            }
+                            else
+                            {
+                                if (correcte)
+                                {
+                                    if (parts[0] == "ORD")
+                                    {
+                                        orders.Add(new Orders { codeOrder = parts[1] });
+
+                                        priority.Add(new Priority { idPriority = orders[0].IdFactory, DescPriority = parts[2] });
+
+                                        sc.Priority.AddRange(priority);
+                                    }
+
+                                    ordersDetail.Add(new OrdersDetail { idOrder = orders[0].idOrder });
+                                    orderInfo.Add(new OrderInfo { idOrder = orders[0].idOrder });
+
+                                    if (parts[0] == "DTM")
+                                    {
+                                        orders.Add(new Orders { idOrder = orders[0].idOrder, dateOrder = DateTime.ParseExact(parts[1], "yyyyMMdd", CultureInfo.InvariantCulture) });
+                                    }
+                                    else if (parts[0] == "NADMS")
+                                    {
+                                        agencies.Add(new Agencies { idAgency = orderInfo[0].idAgency, DescAgency = parts[2] });
+                                        operationalAreas.Add(new OperationalAreas { idOperationalArea = orderInfo[0].idOperationalArea, DescOperationalArea = parts[1] });
+
+                                        sc.Agencies.AddRange(agencies);
+                                        sc.OperationalAreas.AddRange(operationalAreas);
+                                    }
+                                    else if (parts[0] == "NADMR")
+                                    {
+                                        factories.Add(new Factories { idFactory = orders[0].IdFactory, codeFactory = parts[1] });
+
+                                        sc.Factories.AddRange(factories);
+                                    }
+                                    else if (parts[0] == "LIN")
+                                    {
+                                        OrdersDetail newOrderDetail = new OrdersDetail { idOrder = orders[0].idOrder };
+                                        ordersDetail.Add(newOrderDetail);
+
+                                        Planets newPlanet = new Planets { CodePlanet = parts[1] };
+                                        planets.Add(newPlanet);
+
+                                        References newReference = new References { codeReference = parts[2] };
+                                        references.Add(newReference);
+
+                                        sc.Planets.AddRange(planets);
+                                        sc.References.AddRange(references);
+                                    }
+                                    else if (parts[0] == "QTYLIN")
+                                    {
+                                        if (parts[1] == "61")
+                                        {
+                                            cantidad = (int.Parse(parts[2]) * (-1));
+                                        }
+                                        else
+                                        {
+                                            cantidad = int.Parse(parts[2]);
+                                        }
+
+                                        ordersDetail.Add(new OrdersDetail { idOrder = orders[0].idOrder, Quantity = (short)cantidad });
+                                    }
+                                    else if (parts[0] == "DTMLIN")
+                                    {
+                                        ordersDetail.Add(new OrdersDetail { idOrder = orders[0].idOrder, DeliveryDate = DateTime.ParseExact(parts[1], "yyyyMMdd", CultureInfo.InvariantCulture) });
+                                    }
+                                }
+                            }
+                            txt_Edi.Text += line + "\r\n";
+                        }
+                        sc.Orders.AddRange(orders);
+                        sc.OrderInfo.AddRange(orderInfo);
+                        sc.OrdersDetail.AddRange(ordersDetail);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex}");
+            }
+        }
+
+        private void swTextButton2_ButtonClick(object sender, EventArgs e)
+        {
+            try
+            {
+                sc.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex}");
+            }
+        }
+
+    }
+}
