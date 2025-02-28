1600dc8ca367534eccc1992d97e2612262fc76bb
Leandro Varas <leovaras2005@gmail.com>
2025-02-28 17:45:08 +0100

    feat: listar ordenes

---
+++ OrderMaintenanceEF/OrderEF.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace OrderMaintenanceEF
+{
+    partial class OrderEF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxFechaEntrega = new AVISC_Controles.SWTextbox();
+            this.cbPrioridad = new System.Windows.Forms.ComboBox();
+            this.cbAgencia = new System.Windows.Forms.ComboBox();
+            this.cbCodeOrder = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // tbxFechaEntrega
+            // 
+            this.tbxFechaEntrega.AllowEmpty = false;
+            this.tbxFechaEntrega.DadaPermesa = AVISC_Controles.ContentType.Nombre;
+            this.tbxFechaEntrega.IsForeignKey = false;
+            this.tbxFechaEntrega.LinkedControlName = null;
+            this.tbxFechaEntrega.Location = new System.Drawing.Point(197, 51);
+            this.tbxFechaEntrega.Name = "tbxFechaEntrega";
+            this.tbxFechaEntrega.Size = new System.Drawing.Size(100, 22);
+            this.tbxFechaEntrega.TabIndex = 1;
+            // 
+            // cbPrioridad
+            // 
+            this.cbPrioridad.FormattingEnabled = true;
+            this.cbPrioridad.Location = new System.Drawing.Point(326, 51);
+            this.cbPrioridad.Name = "cbPrioridad";
+            this.cbPrioridad.Size = new System.Drawing.Size(121, 24);
+            this.cbPrioridad.TabIndex = 2;
+            // 
+            // cbAgencia
+            // 
+            this.cbAgencia.FormattingEnabled = true;
+            this.cbAgencia.Location = new System.Drawing.Point(479, 51);
+            this.cbAgencia.Name = "cbAgencia";
+            this.cbAgencia.Size = new System.Drawing.Size(121, 24);
+            this.cbAgencia.TabIndex = 3;
+            // 
+            // cbCodeOrder
+            // 
+            this.cbCodeOrder.FormattingEnabled = true;
+            this.cbCodeOrder.Location = new System.Drawing.Point(53, 51);
+            this.cbCodeOrder.Name = "cbCodeOrder";
+            this.cbCodeOrder.Size = new System.Drawing.Size(121, 24);
+            this.cbCodeOrder.TabIndex = 4;
+            // 
+            // OrderEF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbCodeOrder);
+            this.Controls.Add(this.cbAgencia);
+            this.Controls.Add(this.cbPrioridad);
+            this.Controls.Add(this.tbxFechaEntrega);
+            this.Name = "OrderEF";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.OrderEF_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private AVISC_Controles.SWTextbox tbxFechaEntrega;
+        private System.Windows.Forms.ComboBox cbPrioridad;
+        private System.Windows.Forms.ComboBox cbAgencia;
+        private System.Windows.Forms.ComboBox cbCodeOrder;
+    }
+}
---
+++ OrderMaintenanceEF/OrderEF.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AVISC_DatabaseModel;
+using AVIDataAccess;
+
+namespace OrderMaintenanceEF
+{
+    public partial class OrderEF : Form
+    {
+        public OrderEF()
+        {
+            order = new EFDataAccess<Orders>(secureCoreEF);
+            InitializeComponent();
+        }
+
+        SecureCoreEF secureCoreEF = new SecureCoreEF();
+        EFDataAccess<Orders> order;
+
+        private void OrderEF_Load(object sender, EventArgs e)
+        {
+            List<Orders> orders = order.Refresh();
+
+            foreach (Orders order in orders){
+                cbCodeOrder.Items.Add(order.ToString());
+            }
+
+
+        }
+    }
+}
